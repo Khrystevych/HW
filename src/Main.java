5ce2cd773065b8a49f580b720c50cf1852e1d3a6
Khrystevych <123098817+Khrystevych@users.noreply.github.com>
2023-01-29 10:40:11 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class MyClass {
+    int year = 2023;
+    public static int myMethod(int a, int b){
+        int c = a + b;
+        return c;}
+    public static void main(String[] args) {
+        myMethod(num1, num2);}
+}
